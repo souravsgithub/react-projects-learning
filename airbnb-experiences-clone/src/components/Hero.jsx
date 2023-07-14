8112991b141770d192c474c0af21555a1df17b70
Sourav <souravsemail2001@gmail.com>
2023-07-14 21:16:03 +0530

    wrote the react code for navbar and hero in react

---
+++ airbnb-experiences-clone/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import grid from "/image-grid.png";
+
+export default function Hero() {
+    return (
+        <>
+            <img src={grid} alt="" />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind-hosts-all without leaving home.</p>
+        </>
+    );
+}
---
+++ airbnb-experiences-clone/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from "/airbnb-logo.png";
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <img src={logo} alt="airbnb-logo" />
+            </nav>
+        </header>
+    );
+}
