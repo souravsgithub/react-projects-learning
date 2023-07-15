7fbb2554fb747ff73b518810c5b94f40544f7ee9
Sourav <souravsemail2001@gmail.com>
2023-07-16 01:35:37 +0530

    almost done with the info section

--- souravs-business-card/src/components/Info.jsx
+++ souravs-business-card/src/components/Info.jsx
@@ -1,21 +1,23 @@
 import me from "/images/me.png";
+import emailLogo from "/images/email.png"
+import linkedinLogo from "/images/linkedin.png"
 export default function Info() {
     return (
-        <>
-            <img src={me} alt="" />
+        <section className="info">
+            <img src={me} alt="A-photo-of-mine" className="my-photo" />
             <h1>Sourav Kumar</h1>
             <h2>Fullstack Developer</h2>
             <p><a href="#">sourav's portfolio</a></p>
             <div className="info-btns">
                 <a href="#">
-                    <img src="" alt="" />
+                    <img src={emailLogo} alt="" className="small"/>
                     <span>Email</span>
                 </a>
-                <a href="#">
-                    <img src="" alt="" />
+                <a href="#" className="less">
+                    <img src={linkedinLogo} alt="" className="small"/>
                     <span>LinkedIn</span>
                 </a>
             </div>
-        </>
+        </section>
     );
 }
