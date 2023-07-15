0a9e3cff32eef16a5942247889a6c1dd089edfb6
Sourav <souravsemail2001@gmail.com>
2023-07-16 02:30:55 +0530

    made the interests section

--- souravs-business-card/src/App.jsx
+++ souravs-business-card/src/App.jsx
@@ -1,10 +1,13 @@
 import Info from "./components/Info";
 import About from "./components/About";
+import Interests from "./components/Interests";
+
 export default function App() {
   return(
     <section className="main-container">
       <Info />
       <About />
+      <Interests />
     </section>
   );
 }
