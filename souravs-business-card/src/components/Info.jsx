6944d4518de609e6db133499d7291138ce4dd38b
Sourav <souravsemail2001@gmail.com>
2023-07-16 01:59:15 +0530

    final commit for info section

--- souravs-business-card/src/components/Info.jsx
+++ souravs-business-card/src/components/Info.jsx
@@ -7,14 +7,14 @@ export default function Info() {
             <img src={me} alt="A-photo-of-mine" className="my-photo" />
             <h1>Sourav Kumar</h1>
             <h2>Fullstack Developer</h2>
-            <p><a href="#">sourav's portfolio</a></p>
+            <p><a href="sourav-kumar.vercel.app" target="_blank">sourav's portfolio</a></p>
             <div className="info-btns">
-                <a href="#">
-                    <img src={emailLogo} alt="" className="small"/>
+                <a href="mailto: souravsemail2001@gmail.com">
+                    <img src={emailLogo} alt="email-link" className="small"/>
                     <span>Email</span>
                 </a>
-                <a href="#" className="less">
-                    <img src={linkedinLogo} alt="" className="small"/>
+                <a href="https://www.linkedin.com/in/sourav-kumar-79715725a/" target="_blank" className="less">
+                    <img src={linkedinLogo} alt="linkedin-link" className="small"/>
                     <span>LinkedIn</span>
                 </a>
             </div>
