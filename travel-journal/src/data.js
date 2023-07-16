c16b57ead15e17990115d5347fe116d2d93d4f8f
Sourav <souravsemail2001@gmail.com>
2023-07-16 15:10:24 +0530

    added the google maps locations

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -3,7 +3,8 @@ export default [
     id: 1,
     location: "JAPAN",
     title: "Mount Fuji",
-    googleMapsUrl: "",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     imageUrl: "japan.jpg",
@@ -14,7 +15,8 @@ export default [
     id: 2,
     location: "Australia",
     title: "Sydney Opera House",
-    googleMapsUrl: "",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2127164,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0?entry=ttu",
     startDate: "27 May, 2021",
     endDate: "8 Jun, 2021",
     imageUrl: "sydney.jpg",
@@ -25,7 +27,8 @@ export default [
     id: 3,
     location: "NORWAY",
     title: "Geirangerfjord",
-    googleMapsUrl: "",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Geirangerfjord/@62.1048241,6.9926424,12z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi?entry=ttu",
     startDate: "1 Oct, 2021",
     endDate: "18 Nov, 2021",
     imageUrl: "norway.jpg",
