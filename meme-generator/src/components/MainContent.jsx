492707036a204a78073f42128e1643af09e8ced7
Sourav <souravsemail2001@gmail.com>
2023-07-21 13:06:55 +0530

    added the random image changing functionality on button click

--- meme-generator/src/components/MainContent.jsx
+++ meme-generator/src/components/MainContent.jsx
@@ -3,19 +3,39 @@ import memesData from "../memesData.js";
 import { useState } from "react";
 
 export default function MainContent() {
-  console.log(memesData);
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: memeImg,
   });
+
+  const memesArr = memesData.data.memes;
+
+  function handleClick() {
+    let randomNumber = Math.floor(Math.random() * memesArr.length);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: memesArr[randomNumber].url,
+    }));
+  }
+
   return (
     <div className="container">
       <form>
-        <input placeholder="Top Text" />
-        <input placeholder="Bottom Text" />
+        <input
+          placeholder="Top Text"
+          value={meme.topText}
+          onChange={handleChange}
+          name="topText"
+        />
+        <input
+          placeholder="Bottom Text"
+          value={meme.bottomText}
+          onChange={handleChange}
+          name="bottomText"
+        />
       </form>
-      <button>Get a new meme image 🖼</button>
+      <button onClick={handleClick}>Get a new meme image 🖼</button>
       <div className="container-image">
         <img src={meme.randomImage} alt="meme-image" />
         <h4 className="top-text">{meme.topText}</h4>
